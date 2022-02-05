7b49d00ec01f12f4b94a8b3840f6c9140fb91bee
Renato Nogueira <renatopsn@gmail.com>
2022-02-05 13:31:28 -0300

    São José de Ribamar
    Site da cidade São José de Ribamar

---
+++ js/index.js
@@ -0,0 +1,71 @@
+var iniciop = new Vue({
+  el: '#iniciop',
+  data: {
+    inicio: `<h3> A lenda de São José de Riba pro Mar </h3>
+    <p class="lead">Diz a lenda que um navegador português, após ter se desviado de sua rota, esteve prestes a naufragar, em plena baía de São José, por causa de uma tempestade. Tendo invocado a intercessão do santo, o navegador viu a tempestade cessar. </p>
+<br> 
+<p class="lead"> Grato pela ajuda de São José, decidiu erguer uma capelinha de frente para o mar. Para isso, trouxe de Portugal uma imagem de São José. </p><br><br>`
+  }
+});
+
+var txt1 = new Vue({
+    el: '#txt1',
+    data: {
+      lavaPratos: `O Lava-Pratos acontece sempre no final de semana após o carnaval tradicional. O evento é considerado como sendo um dos carnavais fora de época mais antigos do país, além de encerrar oficialmente a temporada momesca no Estado.
+      De acordo com historiadores ribamarenses, o evento teve sua origem no ano de 1946, no então chamado “Carnaval da Vitória”, assim batizado após a vitoriosa participação do Brasil na Segunda Guerra Mundial, que terminou em 1945`
+    }
+  });
+
+  var gruta = new Vue({
+    el: '#gruta',
+    data: {
+      grutadeloude: `
+      <h5>Gruta de Nossa Senhora de Lourdes</h5>
+      <p>
+      Lourdes é uma pequena cidade francesa, localizada aos pés dos Pirineus, a 420m de altitude. Situa-se entre a planície e a montanha. No Santuário de São José de Ribamar, temos uma réplica da Gruta de Lourdes, onde todos os anos milhares de romeiros visitam e fazem suas orações.
+      </p>
+      `
+    }
+  });
+
+  var museu = new Vue({
+    el: '#museu',
+    data: {
+      exvotos: `
+      <h5>Museu dos Ex-votos</h5>
+      <p>
+      Com um acervo catalogado, que possui desde mechas de cabelo, fardamentos militares a réplicas de monumentos, como a igreja matriz, o Museu dos Ex-Votos abriga objetos devocionais. São artefatos que simbolizam a gratidão dos romeiros e devotos de São José de Ribamar pelas graças e benção alcançadas.
+      </p>`
+    }
+  });
+
+  var estatua = new Vue({
+    el: '#estatua',
+    data: {
+      monumento: `
+      <h5>Monumento de São José de Ribamar</h5>
+      <p>Com 33m de altura, o monumento a São José é um dos maiores do país, ficando atrás apenas do Cristo Redentor com 38m. O monumento retrata São José segurando firmemente a mão do Menino Jesus, simbolizando o carinho e a preocupação de ensinar os primeiros passos ao Menino Jesus.</p>`
+    }
+  });
+
+  var igreja = new Vue({
+    el: '#igreja',
+    data: {
+      igrejasjr: `
+      <h5>Igreja de São José de Ribamar</h5>
+      <p>
+      A Igreja de São José de Ribamar é um templo católico dedicado ao santo padroeiro do Maranhão. A igreja atual, construída em 1915 de frente para a orla é considerada um símbolo da cidade.
+      </p>`
+    }
+  });
+
+  var casaMilagres = new Vue({
+    el: '#cmilagres',
+    data: {
+      cdosMilagres: `
+      <h5>Casa dos Milagres</h5>
+      <p>
+      Idealizada para abrigar objetos dedicados a São José de Ribamar. Após a grande reforma de 2001, as peças foram realocadas para o Museu dos ex-votos e o prédio foi restaurado para abrigar a loja oficial do Santuário de São José de Ribamar.
+      </p>`
+    }
+  });
---
+++ js/modal.js
@@ -0,0 +1,65 @@
+  var lenda = new Vue({
+    el: '#lenda',
+    data: {
+      alenda: `<p class="captular">Diz a lenda que um navegador português, após ter se desviado de sua rota, esteve prestes a naufragar, em plena baía de São José, por causa de uma tempestade. Tendo invocado a intercessão do santo, o navegador viu a tempestade cessar. Grato pela ajuda de São José, decidiu erguer uma capelinha de frente para o mar. Para isso, trouxe de Portugal uma imagem de São José.</p>
+
+      <p>Tempos depois, sem que ninguém percebesse, os moradores de Anindiba dos Indígenas, hoje Paço do Lumiar, levaram a imagem para a igreja do povoado. Contudo, misteriosamente, ao amanhecer, notaram que a imagem de São José voltara à sua capela de origem. Mais uma vez os moradores de Anindiba levaram a imagem para sua igreja e colocaram sentinelas para tomar conta. Mas, São José, depois de ter transformado seu cajado em luzeiro, desceu, protegido por santos e anjos, de Anindiba para a capela de Ribamar.</p>
+
+      <img src="../img/sao-jose-de-ribamar.jpg" alt="São José de Ribamar" class="img img-fluid">
+      <figcaption class="figure-caption text-right"><em>Visão aérea da Basílica de São José de Ribamar</em></figcaption>
+<br><br>
+      <p>O caminho por onde São José passou ficou repleto de rastros de luz. Assim, os moradores de Anindiba entenderam que São José desejava ficar em sua capela de frente para o mar. Conta-se ainda, que, tempos depois, a capela de São José foi reconstruída de frente para a entrada da cidade, contudo, as paredes da nova igreja ruíram inúmeras vezes. Só então os devotos perceberam que a igreja deveria ficar como antes, de frente para o mar. O atual prédio da igreja matriz teve a construção iniciada em março de 1915, sendo concluído dois anos mais tarde.</p>
+ 
+      <p>Paróquia São José de Ribamar - Em 29 de abril de 1942, Dom Carlos Carmello de Vasconcellos Motta, arcebispo de São Luís do Maranhão, elevou o curato (aldeia, povoado) de São José de Ribamar à categoria de Paróquia amovível com o mesmo orago (santo padroeiro) e a mesma Matriz.</p>
+
+      <p>Santuário Arquidiocesano São José de Ribamar - Em 09 de setembro de 2011, por ocasião da abertura do Festejo do Glorioso São José de Ribamar, Dom Frei José Belisário da Silva, OFM, arcebispo de São Luís do Maranhão, criou o Santuário Arquidiocesano São José de Ribamar.</p>
+        
+      <p>O Complexo Santuário é composto por: Igreja Matriz de São José de Ribamar, Centro Pastoral, Salão Paroquial, Casa dos Milagres, Praça São José (caminho de São José), Concha Acústica, Cripta (sob a Concha), Gruta de Nossa Senhora de Lourdes, Monumento a São José, Museu dos ex-votos.</p>`
+    }
+  });
+
+  var saojose = new Vue({
+    el: '#saojose',
+    data: {
+      sjr: `
+      <img src="../img/monumento-sao-jose-de-ribamar.jpg" alt="São José de Ribamar" class="img img-fluid">
+      <figcaption class="figure-caption text-right"><em>Em março de 1997, foi iniciada a construção do monumento e sua inauguração ocorreu em 04 de agosto de 1998.</em></figcaption></p>`
+    }
+  });
+
+  var migreja = new Vue({
+    el: '#migreja',
+    data: {
+      migrejasjr: `
+      <img src="../img/igrejabiaman2.jpg" alt="São José de Ribamar" class="img img-fluid">
+      <figcaption class="figure-caption text-right"><em>Igreja atua de São José de Ribamar.</em></figcaption></p>`
+    }
+  });
+
+  var mgruta = new Vue({
+    el: '#mgruta',
+    data: {
+      mlourdes: `
+      <img src="../img/gruta-nossa-senhora-de-lourdes.jpg" alt="São José de Ribamar" class="img img-fluid">
+      <figcaption class="figure-caption text-right"><em> Réplica da Gruta de Lourdes, onde todos os anos milhares de romeiros visitam e fazem suas orações</em></figcaption></p>`
+    }
+  });
+
+  
+  var mmuseu = new Vue({
+    el: '#mmuseu',
+    data: {
+      mexvotos: `
+      <img src="../img/museu.jpg" alt="São José de Ribamar" class="img img-fluid">
+      <figcaption class="figure-caption text-right"><em> Réplica da Gruta de Lourdes, onde todos os anos milhares de romeiros visitam e fazem suas orações</em></figcaption></p>`
+    }
+  });
+
+  var mCasaMilagre = new Vue({
+    el: '#mcasamilagre',
+    data: {
+      mMilagres: `
+      <img src="../img/casa-dos-milagres.jpg" alt="São José de Ribamar" class="img img-fluid">
+      <figcaption class="figure-caption text-right"><em> O Salão das Graças, primeiro nome dado ao espaço, foi construído em 1895 e, em pouco tempo, foi tomado por peças de pessoas de vários lugares do Maranhão.</em></figcaption></p>`
+    }
+  });
